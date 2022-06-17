9fb2e75e1feaa87d3fcd50430bea9e3e90f3fa80
Rafael Mindreau <rafael.mindreau@gmail.com>
2022-06-17 19:12:54 +0200

    Upgrade config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,26 @@
 module.exports = {
   siteMetadata: {
     title: `EZAC`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://ezac.nl`,
+    description: `Zweefvliegen in Zeeuws Vlaanderen.`,
+    author: `@ezac-dream-team`,
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: "Eerste Zeeuws-Vlaamse Aero Club",
+        short_name: "EZAC",
+        start_url: `https://www.ezac.nl`,
+        background_color: "#003082",
+        theme_color: "#0469FF",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/img/EZAC_logo.svg" // This path is relative to the root of the site.
+      },
+    },
+  ],
 };
