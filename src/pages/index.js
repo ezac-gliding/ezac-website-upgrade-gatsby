80ab37f1d4747fd8747bde36ca6cfb7ef33e8a7d
Rafael Mindreau <rafael.mindreau@gmail.com>
2023-02-14 17:22:05 +0100

    Complete overhaul of homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,39 @@ import Page from 'src/components/UI/Page';
 import Hero from 'src/components/hero/Hero';
 import ClubValue from 'src/components/home/ClubValue';
 import useScroll from 'src/hooks/useScroll';
+import ButtonLink from 'src/components/button/ButtonLink';
+import BubbleOne from 'components/floating-elements/bubbles/BubbleOne';
+import BubbleTwo from 'components/floating-elements/bubbles/BubbleTwo';
+import BubbleThree from 'components/floating-elements/bubbles/BubbleThree';
+import BigSolidBubble from 'components/floating-elements/bubbles/BigSolidBubble';
+import WavyBubble from 'components/floating-elements/bubbles/WavyBubble';
+import AngleBubble from 'components/floating-elements/bubbles/AngleBubble';
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+import { useStaticQuery, graphql } from 'gatsby';
 import './home.scss';
 import 'src/styles/reset.scss';
 import 'src/styles/general.scss';
 
+const homepageTextsQuery = graphql`
+query {
+  allHomepageTextsJson {
+    nodes {
+      title
+      text
+      id
+    }
+  }
+}
+`;
+
 export default function Index() {
   const scrollPosition = useScroll();
+
+  const {
+    allHomepageTextsJson: {
+      nodes: homepageTexts,
+    },
+  } = useStaticQuery(homepageTextsQuery);
 
   const valueStyle = useMemo(() => ({
     opacity: scrollPosition > 220 ? 1 : 0,
@@ -19,7 +46,7 @@ export default function Index() {
 
   return (
     <div className="home">
-      <Header appearAt={290} />
+      <Header appearAt={140} />
       <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0469FF" />
@@ -29,53 +56,106 @@ export default function Index() {
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@1,700&family=Open+Sans:wght@400;800&display=swap" rel="stylesheet" />
         <title>EZAC</title>
       </Helmet>
 
-      <Hero appearAt={80} scrollPosition={scrollPosition} />
+      <ParallaxProvider>
+        <Page>
+          <Parallax
+            style={{ top: '320px', left: '50px' }}
+            className="floating-bubble"
+            speed={-16}
+            rotate={[-24, 10]}
+          >
+            <BubbleTwo scale="30vw" fill="#9DC4FF" accent="white" />
+          </Parallax>
 
-      <Page>
-        <div style={valueStyle} className="club-values">
-          <ClubValue appearAt={120} title="Teamwork">
-            Een sport zoals zweefvliegen kan niet bestaan zonder
-            <strong> een goede samenwerking tussen clubleden. </strong>
-          </ClubValue>
-          <ClubValue appearAt={120} title="Instructie">
-            Er is
-            <strong> geen extra kost voor instructie. </strong>
-            Je hoeft ook niet op voorhand de theorie te volgen. In het zweefvliegen stap je samen met instructeur in een tweezitter, en ga je dus direct aan de slag met de opleiding.
-          </ClubValue>
-          <ClubValue appearAt={120} title="Minutengeld">
-            Het lidgeld is een
-            <strong> all-in-formule, </strong>
-            wat wilt zeggen dat je niet betaalt voor starts, noch voor het aantal minuten die je vliegt op een bepaald type
-          </ClubValue>
-        </div>
+          <Parallax
+            style={{ top: '-210px', left: '-120px' }}
+            className="floating-bubble"
+            speed={-28}
+            rotate={[-40, 40]}
+          >
+            <BubbleOne scale="30vw" fill="#244059" accent="white" />
+          </Parallax>
 
-        <h2>Welkom op de EZAC</h2>
-        <h3>Onze club</h3>
-        <p>
-          EZAC, oftewel de Eerste Zeeuws-Vlaamse Aero Club, is een zweefvliegclub met als thuisveld Axel.
-          We hebben een schitterend vliegveld gelegen in het natuurgebied de Smitschorre, een flinke vloot
-          met de modernste zweefvliegtuigen, een gezellige kantine en een eigen verenigingscamping.
-        </p>
+          <Parallax
+            style={{ top: '420px', right: '-20px' }}
+            className="floating-bubble"
+            speed={-10}
+            rotate={[24, -10]}
+          >
+            <AngleBubble scale="14vw" fill="#FFF1A7" />
+          </Parallax>
 
-        <h3>Meevliegen</h3>
-        <p>
-          Als u wilt, kunt met een van onze ervaren vliegers meevliegen, of iemand anders een doopvlucht cadeau doen.
-          Reserveer tijdig en op voorhand, want de beschikbare timeslots gaan er snel door.
-        </p>
+          <Parallax
+            style={{ top: '80px', right: '-40px' }}
+            className="floating-bubble"
+            speed={-20}
+            rotate={[24, -10]}
+          >
+            <BigSolidBubble scale="30vw" fill="#9DC4FF" />
+          </Parallax>
 
-        <h3>Leer zelf zweefvliegen</h3>
-        <p>
-          Zweefvliegen is een geweldige en voordelige manier van vliegen. Zonder motor vliegen we op de kracht
-          van de natuur van thermiekbel naar thermiekbel, soms uren en honderden kilometers ver. Ook zonder wind en thermiek
-          kan een zweefvliegtuig vliegen, maar dan niet zo lang. Bij ons kan u genieten van een volledige opleiding.
-          Je stapt in met een instructeur die je wegwijs maakt in deze prachtige sport. De EZAC is er voor zowel Nederlandse
-          als Belgische zweefvliegers.
-        </p>
-      </Page>
+          <Parallax
+            style={{ top: '160px', right: '-80px' }}
+            className="floating-bubble"
+            speed={-10}
+            rotate={[24, -10]}
+          >
+            <WavyBubble scale="13vw" fill="white" />
+          </Parallax>
+
+          <Parallax
+            style={{ top: '-290px', right: '-140px' }}
+            className="floating-bubble"
+            speed={-29}
+            rotate={[24, -10]}
+          >
+            <BubbleThree scale="25vw" fill="#4C6CA4" accent="white" />
+          </Parallax>
+
+          <Hero />
+
+          <p className="call-to-action-text">
+            Leer&nbsp;
+            <strong>vliegen</strong>
+            &nbsp;aan de EZAC
+          </p>
+
+          <ButtonLink href="./prices" className="cta-button">Ontdek onze tarieven</ButtonLink>
+
+          <div style={valueStyle} className="club-values">
+            <ClubValue title="Teamwork">
+              Een sport zoals zweefvliegen kan niet bestaan zonder
+              <strong> een goede samenwerking tussen clubleden. </strong>
+            </ClubValue>
+            <ClubValue title="Instructie">
+              Er is
+              <strong> geen extra kost voor instructie. </strong>
+              Je hoeft ook niet op voorhand de theorie te volgen. In het zweefvliegen stap je samen met instructeur in een tweezitter, en ga je dus direct aan de slag met de opleiding.
+            </ClubValue>
+            <ClubValue title="Minutengeld">
+              Het lidgeld is een
+              <strong> all-in-formule, </strong>
+              wat wilt zeggen dat je niet betaalt voor starts, noch voor het aantal minuten die je vliegt op een bepaald type
+            </ClubValue>
+          </div>
+
+          <h2>Welkom op de EZAC</h2>
+          {
+            homepageTexts.map(({ id, title, text }) => (
+              <div key={id} className="homepage-text">
+                <h3>{title}</h3>
+                <p>
+                  {text}
+                </p>
+              </div>
+            ))
+          }
+        </Page>
+      </ParallaxProvider>
     </div>
   );
 }
