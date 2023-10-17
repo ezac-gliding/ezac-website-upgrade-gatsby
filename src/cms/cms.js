de00639c16f32bd1f96eb0665ece279dafa2b68b
rafael.mindreau <rafael.mindreau@gmail.com>
2023-10-17 15:05:46 +0200

    Fix bug with text editor widget cursor jumping to end of text on change

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -32,3 +32,13 @@ CMS.registerPreviewTemplate('glider', GliderPreview);
 CMS.registerPreviewTemplate('homepage-text', HomepageTextPreview);
 CMS.registerPreviewTemplate('club-stories', ClubTextPreview);
 CMS.registerPreviewTemplate('prices', PricePreview);
+
+// Allow to change CMS styles (non-preview)
+if (typeof window !== 'undefined') {
+  // add admin.css
+  const link = document.createElement('link');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  link.href = '/admin/admin.css';
+  document.head.appendChild(link);
+}
