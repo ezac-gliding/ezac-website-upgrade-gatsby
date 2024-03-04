4bf9d85c1a614fea43208c4e9c3f1735e8d6330a
Rafael Mindreau <rafael.mindreau@gmail.com>
2024-03-04 20:05:50 +0100

    Upgrade youtube URL in accordance with cookie policy

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import useCookie from 'hooks/useCookie';
 import './Hero.scss';
 
+const videoUrl = 'q4AEXXGz4b8';
+
 export default () => {
   const { getCookie } = useCookie('cookie-consent');
   const [youtubeCookies, setYoutubeCookies] = useState(false);
@@ -15,9 +17,9 @@ export default () => {
       <div className="video-container">
         {
           youtubeCookies ? (
-            <iframe src="https://www.youtube.com/embed/USQGCsMXU0U?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=USQGCsMXU0U" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+            <iframe src={`https://www.youtube.com/embed/${videoUrl}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&vq=hd1080`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
           ) : (
-            <iframe src="https://www.youtube-nocookie.com/embed/USQGCsMXU0U?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=USQGCsMXU0U" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+            <iframe src={`https://www.youtube-nocookie.com/embed/${videoUrl}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&vq=hd1080`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
           )
         }
       </div>
