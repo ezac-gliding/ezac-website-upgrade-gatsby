908089a363eca5769b97c703e1733fb629d74355
Rafael Mindreau <rafael.mindreau@gmail.com>
2024-03-04 20:14:43 +0100

    Fix video not repeating

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -17,9 +17,9 @@ export default () => {
       <div className="video-container">
         {
           youtubeCookies ? (
-            <iframe src={`https://www.youtube.com/embed/${videoUrl}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&vq=hd1080`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+            <iframe src={`https://www.youtube.com/embed/${videoUrl}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&vq=hd1080&playlist=${videoUrl}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
           ) : (
-            <iframe src={`https://www.youtube-nocookie.com/embed/${videoUrl}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&vq=hd1080`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+            <iframe src={`https://www.youtube-nocookie.com/embed/${videoUrl}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&vq=hd1080&playlist=${videoUrl}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
           )
         }
       </div>
